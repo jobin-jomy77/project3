651c9ec01ed52a646214681233a21eadd8909a18
Jobin Jomy Mathew <jerryjoben@gmail.com>
2025-07-29 23:28:28 +0530

    first commit

---
+++ p1/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+//import 'screens/input_screen.dart';
+
+void main() {
+  runApp(TourApp());
+}
+
+class TourApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tour Recommendation',
+      theme: ThemeData(primarySwatch: Colors.teal),
+    );
+  }
+}
